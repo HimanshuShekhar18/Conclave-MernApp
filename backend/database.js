1e9fe0f47b689f64a2bdefdfce22a28fa6e79972
Himanshu <shekhar.iiitian@gmail.com>
2024-01-20 04:33:28 +0530

    Deployment and AWS

--- backend/database.js
+++ backend/database.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 function DbConnect() {
-    const DB_URL = process.env.DB_URL;
+
     // Database connection
-    mongoose.connect(DB_URL);
+    mongoose.connect(process.env.DB_URL);
     const db = mongoose.connection;
 
     db.on('error', console.error.bind(console, 'connection error:'));
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,6 +8,6 @@ import { Provider } from 'react-redux';
 
 const root = document.getElementById('root');
 const rootElement = ReactDOM.createRoot(root);
-rootElement.render(<Provider store ={store}><App /></Provider>);
+rootElement.render(<Provider store ={store}><App /></Provider>);  // removed strict mode
 // pass the store as a prop:
 reportWebVitals();
